b63ebf7dbf19e8f3c3df61f72b70ef841c8d707e
barve-sneha26 <barve.sneha26@outlook.com>
2024-11-17 18:49:35 -0500

    Data Ingestion done

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.st_1_data_ingestion_pipeline import DataIngestionPipeline 
 
-logger.info("Logging is implemented")
+# logger.info("Logging is implemented")
 
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f"Stage {STAGE_NAME} initiated") 
+    data_ingestion_pipeline = DataIngestionPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e 
---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PAth = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")  
+
